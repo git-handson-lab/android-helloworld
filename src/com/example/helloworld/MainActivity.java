75fd523ca29f4a485b3e52eac4dc4f19b3215e75
Rym Zrelli <rym.zrelli@gmail.com>
2014-08-30 12:42:10 +0100

    supprimer import dans MainActivity.java

--- src/com/example/helloworld/MainActivity.java
+++ src/com/example/helloworld/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.helloworld;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+
 
 public class MainActivity extends Activity {
 
